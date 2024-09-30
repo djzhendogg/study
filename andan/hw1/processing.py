156d047d86de76ea62a959bade9f391bb71e462b
djzhendogg <djzhendogg.is.busy2@gmail.com>
2024-09-30 22:33:11 +0300

    prog.andan.1.1

---
+++ andan/hw1/processing.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from processing_tools import (
+    add_flav_columns,
+    make_list_flavor
+)
+
+
+df = pd.read_csv('perfume.tsv',  sep='\t')
+
+# парсинг делался по компании и названию, следовательно эти строки полностью фиктивны
+df.drop(df[df['company'].isnull()].index, inplace=True)
+df.drop(df[df['name'].isnull()].index, inplace=True)
+# если неизвестно чем пахнут, то не на чем предсказывать
+df.drop(df[df['flavor_group'].isnull()].index, inplace=True)
+# сновные фичи для предсказания, если нул, то строки фиктивны
+df.drop(df[df['upper_notes'].isnull() & df['heart_notes'].isnull() & df['base_notes'].isnull()].index, inplace=True)
+
+# заполнение пропусков
+df['year'].fillna((df['year'].mean()), inplace=True)
+df['expiration, mounth'].fillna((df['expiration, mounth'].mean()), inplace=True)
+df['manufacturer'].fillna('no', inplace=True)
+df['country'].fillna('no', inplace=True)
+
+add_flav_columns('flavor_group')
+
+complex_upper_notes = make_list_flavor('upper_notes')
+complex_heart_notes = make_list_flavor('heart_notes')
+complex_base_notes = make_list_flavor('base_notes')
+
+all_notes = list(set(complex_upper_notes+complex_heart_notes+complex_base_notes))
+
+all_flavor_group_dict_cols = {}
+# df_col_flavor_group = df['upper_notes']
+for flavor in all_notes:
+    df_col_list = []
+    for _id in range(df.shape[0]):
+        line = df.iloc[_id]
+        upper_note = line['upper_notes']
+        heart_note = line['heart_notes']
+        base_note = line['base_notes']
+        if pd.notna(upper_note) and flavor in upper_note:
+            df_col_list.append(3)
+        elif pd.notna(heart_note) and flavor in heart_note:
+            df_col_list.append(2)
+        elif pd.notna(base_note) and flavor in base_note:
+            df_col_list.append(1)
+        else:
+            df_col_list.append(0)
+
+    all_flavor_group_dict_cols[flavor] = df_col_list
+
+all_flavor_group_df = pd.DataFrame.from_dict(all_flavor_group_dict_cols)
+
+df = pd.concat([df, all_flavor_group_df], axis=1)
+
+df.drop(['upper_notes', 'heart_notes', 'base_notes', 'flavor_group'], axis=1, inplace=True)
+
+df.to_csv('perfume_processed.csv')
---
+++ andan/hw1/processing_tools.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+
+def make_list_flavor(df, col_name):
+    complex_flavor_group = list(df[col_name].unique())
+    all_flavor_group = []
+    for complex_flavor in complex_flavor_group:
+        if pd.notna(complex_flavor):
+            flavors = complex_flavor.split(';')
+            for flavor in flavors:
+                if flavor not in all_flavor_group:
+                    all_flavor_group.append(flavor)
+    return all_flavor_group
+
+
+def extract_columns(df, col_name):
+    all_flavor_group = make_list_flavor(col_name)
+
+    all_flavor_group_dict_cols = {}
+    df_col_flavor_group = df['upper_notes']
+    for flavor in all_flavor_group:
+        df_col_list = []
+        for df_col in df_col_flavor_group:
+            if pd.notna(df_col):
+                if flavor in df_col:
+                    df_col_list.append(1)
+                else:
+                    df_col_list.append(0)
+            else:
+                df_col_list.append(0)
+        all_flavor_group_dict_cols[flavor] = df_col_list
+    return all_flavor_group_dict_cols
+
+
+def add_flav_columns(df, col_name):
+    flavor_group_columns = extract_columns(col_name)
+    for i in flavor_group_columns.keys():
+        df[i] = flavor_group_columns[i]
