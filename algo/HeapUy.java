3a764118e9122b223608607f1b7f3e2d03407312
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2024-10-31 18:10:44 +0300

    algo.3.2.1

--- algo/HeapUy.java
+++ algo/HeapUy.java
@@ -1,8 +1,21 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class HeapUy {
     public static void main(String[] args) {
-
+        List<Integer> arr = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            int operation = in.nextInt();
+            if (operation == 0) {
+                int x = in.nextInt();
+                insert(arr, x);
+            } else if (operation == 1) {
+                System.out.println(extract(arr));
+            }
+        }
     }
 
     public static void swap(List<Integer> arr, int f, int s) {
@@ -15,6 +28,7 @@ public class HeapUy {
         int parent = (pos - 1) / 2;
         while (arr.get(parent) < arr.get(pos)) {
             swap(arr, parent, pos);
+            pos = parent;
             parent = (parent - 1) / 2;
         }
     }
@@ -27,7 +41,7 @@ public class HeapUy {
             if (childRight < arr.size() && arr.get(childLeft) < arr.get(childRight)) {
                 swapWith = childRight;
             }
-            if (arr.get(pos) >= arr.get(childRight)) {
+            if (arr.get(pos) >= arr.get(swapWith)) {
                 break;
             }
             swap(arr, pos, swapWith);
