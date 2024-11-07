6ece613366af468c7b6b5c77cf16f10dfd10eaf8
Deimoz <davir222012@gmail.com>
2024-11-07 13:00:07 +0300

    prog.9.1.1

--- prog/hw9/md2html/MarkDownBlockReader.java
+++ prog/hw9/md2html/MarkDownBlockReader.java
@@ -19,18 +19,13 @@ public class MarkDownBlockReader {
     }
 
     public AbstractTextDecorator nextBlock() throws IOException {
-        // хранит готовый литс тексовых элементов
         List<TextElement> rowParagraph;
-        // выделяет блок как хедер, тк по разному парсятся
-        boolean isHeader = false;
-        // уровень хедера, также используется для скипа элементов
-        int headerLevel = 0;
-        // хранит итоговый класс разметки заполненный
         AbstractTextDecorator finalParagraph;
+        boolean isHeader = false;
+        int headerLevel = 0;
 
         StringBuilder block = readBlock();
-
-        // обработка содержательных строк
+        block = toEscapedString(block);
         if (block.toString().startsWith("#")) {
             while (block.charAt(headerLevel) == '#') {
                 headerLevel++;
@@ -41,16 +36,23 @@ public class MarkDownBlockReader {
         }
 
         if (isHeader) {
-            // забирает сабстрингу
-            rowParagraph = lineChecker(block.substring(headerLevel + 1));
+            rowParagraph = transformToTextElements(block.substring(headerLevel + 1));
             finalParagraph = new Header(rowParagraph, headerLevel);
         } else {
-            // забирает целую
-            rowParagraph = lineChecker(block.toString());
+            rowParagraph = transformToTextElements(block.toString());
             finalParagraph = new Paragraph(rowParagraph);
         }
 
         return finalParagraph;
+    }
+
+    public boolean hasNext() throws IOException {
+        return !closed;
+    }
+
+    public void close() throws IOException {
+        reader.close();
+        closed = true;
     }
 
     private StringBuilder readBlock() throws IOException {
@@ -69,7 +71,6 @@ public class MarkDownBlockReader {
             if (previosLine) {
                 textBlock.append("\n");
             }
-            line = toEscapedString(line);
             previosLine = true;
             textBlock.append(line);
             line = reader.readLine();
@@ -81,131 +82,42 @@ public class MarkDownBlockReader {
         return textBlock;
     }
 
-    public boolean hasNext() throws IOException {
-        return !closed;
-    }
-
-    public void close() throws IOException {
-        reader.close();
-        closed = true;
-    }
-
-    public String toEscapedString(String str) {
-        StringBuilder escapedStr = new StringBuilder();
-        int start = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '&') {
-                escapedStr.append(str, start, i);
-                escapedStr.append("&amp;");
-                start = i + 1;
-            } else if (str.charAt(i) == '<') {
-                escapedStr.append(str, start, i);
-                escapedStr.append("&lt;");
-                start = i + 1;
-            } else if (str.charAt(i) == '>') {
-                escapedStr.append(str, start, i);
-                escapedStr.append("&gt;");
-                start = i + 1;
-            }
-        }
-        escapedStr.append(str, start, str.length());
-        return escapedStr.toString();
-        // TODO: стрингбилдером сабстрингу + экранированный чар
-//        return str.replace("&", "&amp;")
-//                .replace("<", "&lt;")
-//                .replace(">", "&gt;");
-    }
-
-    private List<TextElement> lineChecker(String line) {
-        // для просмотра в стеке
-        Set<Pattern> patterns = new HashSet<>();
-        // для хранения вложенных структур
-        List<TextBlock> textBlocks = new ArrayList<>();
-        // добвляю базовый токен в котором будет хранится итоговая запись
-        textBlocks.add(new TextBlock(null));
-        // для расинга блоков текста (типа start)
+    private List<TextElement> transformToTextElements(String line) {
+        TokenQueue tokenQueue = new TokenQueue();
         int pointer = 0;
 
         for (int i = 0; i < line.length(); i++) {
-            // хранит паттерн в текущем проходе
-            Pattern nowP;
+            Pattern currentPattern;
             // чек экранирования
             if (line.charAt(i) == '\\') {
-                // финт для удаления экранирования, плохо что если встретилось два экранировани в строке, съест оба
-                line = line.replace("\\", "");
+                line = line.substring(0, i) + line.substring(i + 1);
                 continue;
             }
             // находит токен прохода, если нет, возвращает null
             // TODO: заменить на проход по токенам
             if (i + 1 < line.length()) {
-                nowP = whatToken(line.charAt(i), line.charAt(i + 1));
+                currentPattern = whatToken(line.charAt(i), line.charAt(i + 1));
             } else {
                 // TODO: replace '\u0000'
-                nowP = whatToken(line.charAt(i), '\u0000');
+                currentPattern = whatToken(line.charAt(i), '\u0000');
             }
 
-            if (nowP == null) {
+            if (currentPattern == null) {
                 continue;
             }
 
-            // двигает i чтоб не жрать символы разметки
-            i += nowP.getLen();
-            // вносит блок теста до/между знаками разметки
-            textBlocks.getLast().addElement(new Text(line.substring(pointer, i - nowP.getLen())));
+            i += currentPattern.getLen();
+            tokenQueue.addRowText(line.substring(pointer, i - currentPattern.getLen()));
             pointer = i + 1;
-            // добавляю патерн в сет, first это типа not exist
-            // TODO: выделить в отдельны класс с tokens patterns
-            boolean first = patterns.add(nowP);
-            if (first) {
-                // если встречается первый раз, добавляю в стек
-                textBlocks.add(new TextBlock(nowP));
-            } else {
-                // если уже был, ищу номер токена в листе
-                int startTokenId = findToken(textBlocks, nowP);
-                // На случай фиктивных токенов, типа *_-* собирает все элемента на уровень *
-                for (int j = textBlocks.size() - 1; j > startTokenId; j--) {
-                    // верх стека
-                    // эл ниже
-
-                    // TODO: сделать while сконца c if на == nowP, когда нахожу, оборачиваю и сохраняю, нет - обычную.
-                    // TODO: переделать на дэк
-                    textBlocks.get(j - 1).addElements(textBlocks.get(j).getTextElementList());
-                    // удаляю верх из патернов, чищу кеш патернов
-                    patterns.remove(textBlocks.get(j).getPattern());
-                    // удаляю верх из токенов
-                    textBlocks.remove(j);
-                }
-                // -* * - так как * закрыт, переношу все элементы в обертке на уровень ниже
-                TextElement nowPatterTextElement = nowP.create(textBlocks.get(startTokenId).getTextElementList());
-                textBlocks.get(startTokenId - 1).addElement(nowPatterTextElement);
-
-                patterns.remove(nowP);
-                textBlocks.remove(startTokenId);
+            boolean success = tokenQueue.add(currentPattern);
+            if (success) {
+                continue;
             }
+            tokenQueue.pushDown(currentPattern);
         }
-        // итерации закончились, работаю с ниж уровнем
-        // TODO: переделать на вайл выше
-        TextBlock finalTextBlock = textBlocks.getFirst();
-        // если стек не пуст, за искл нижнего, переношу все вниз
-        for (int j = 1; j < textBlocks.size(); j++) {
-            TextBlock currentTextBlock = textBlocks.get(j);
-            // так как одиночные символы фиктивны, вставляю обратно в текст
-            finalTextBlock.addElement(new Text(currentTextBlock.getPattern().getSeparator()));
-            // перемещаю токены в последний
-            finalTextBlock.addElements(currentTextBlock.getTextElementList());
-        }
-        // забираю остаток
-        finalTextBlock.addElement(new Text(line.substring(pointer)));
-        return finalTextBlock.getTextElementList();
-    }
-
-    private int findToken(List<TextBlock> textBlocks, Pattern desiredPattern) {
-        for (int i = textBlocks.size() - 1; i >= 0; i--) {
-            if (textBlocks.get(i).getPattern() == desiredPattern) {
-                return i;
-            }
-        }
-        return 0;
+        tokenQueue.removeDummyNesting(null);
+        tokenQueue.addRowText(line.substring(pointer));
+        return tokenQueue.getFistTokenList();
     }
 
     private Pattern whatToken(char c, char nextC) {
@@ -228,5 +140,27 @@ public class MarkDownBlockReader {
         } else {
             return null;
         }
+    }
+
+    private StringBuilder toEscapedString(StringBuilder str) {
+        StringBuilder escapedStr = new StringBuilder();
+        int start = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '&') {
+                escapedStr.append(str, start, i);
+                escapedStr.append("&amp;");
+                start = i + 1;
+            } else if (str.charAt(i) == '<') {
+                escapedStr.append(str, start, i);
+                escapedStr.append("&lt;");
+                start = i + 1;
+            } else if (str.charAt(i) == '>') {
+                escapedStr.append(str, start, i);
+                escapedStr.append("&gt;");
+                start = i + 1;
+            }
+        }
+        escapedStr.append(str, start, str.length());
+        return escapedStr;
     }
 }
---
+++ prog/hw9/md2html/TokenQueue.java
@@ -0,0 +1,69 @@
+package md2html;
+
+import md2html.markup.Text;
+import md2html.markup.TextElement;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class TokenQueue {
+    Set<Pattern> patterns;
+    List<TextBlock> textQueue;
+
+    public TokenQueue() {
+        patterns = new HashSet<>();
+        textQueue = new ArrayList<>();
+        textQueue.add(new TextBlock(null));
+    }
+
+    public boolean add(Pattern p) {
+        boolean success = patterns.add(p);
+        if (success) {
+            textQueue.add(new TextBlock(p));
+        }
+        return success;
+    }
+
+    public void pushDown(Pattern desiredPattern) {
+        int startTokenId = findToken(desiredPattern);
+
+        removeDummyNesting(desiredPattern);
+
+        TextElement desiredToken = desiredPattern.create(textQueue.get(startTokenId).getTextElementList());
+        textQueue.get(startTokenId - 1).addElement(desiredToken);
+
+        patterns.remove(desiredPattern);
+        textQueue.remove(startTokenId);
+    }
+
+    public void addRowText(String str) {
+        textQueue.getLast().addElement(new Text(str));
+    }
+
+    public void removeDummyNesting(Pattern desiredPattern) {
+        int index = textQueue.size() - 1;
+
+        while (textQueue.get(index).getPattern() != desiredPattern) {
+            TextBlock toMove = textQueue.get(index);
+            textQueue.get(index - 1).addElements(toMove.getTextElementList());
+            patterns.remove(toMove.getPattern());
+            textQueue.remove(index);
+            index--;
+        }
+    }
+
+    public List<TextElement> getFistTokenList() {
+        return textQueue.getFirst().getTextElementList();
+    }
+
+    private int findToken(Pattern desiredPattern) {
+        for (int i = textQueue.size() - 1; i >= 0; i--) {
+            if (textQueue.get(i).getPattern() == desiredPattern) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
