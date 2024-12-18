929522a530fa2130680550294cc9777b538a4816
djzhendogg <djzhendogg.is.busy2@gmail.com>
2024-12-19 00:52:33 +0300

    tests

--- run_hw12.sh
+++ run_hw12.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro/java-solutions/expression/*.java prog-intro/java-solutions/expression/parser/*.java
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro/java-solutions/expression/*.java
 
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/ToMiniString.java
-
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/Expression.java
-
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/ExpressionTest.java
 javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/ExpressionTester.java
 javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/TripleExpression.java
 java -ea -cp ./prog-intro/java-solutions:./prog-intro-2024/java expression.TripleExpression hard Triple
 
-
-java -cp ./prog-intro/java-solutions game.Main
-
 javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/parser/TripleParser.java
 javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/parser/ParserTest.java
 java -ea -cp ./prog-intro/java-solutions:./prog-intro-2024/java expression.parser.ParserTest easy Base
+java -ea -cp ./prog-intro/java-solutions:./prog-intro-2024/java expression.parser.ParserTest easy LastVars
+java -ea -cp ./prog-intro/java-solutions:./prog-intro-2024/java expression.parser.ParserTest easy Cbrt
 
 
 javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/exceptions/TripleParser.java
--- run_hw15.sh
+++ run_hw15.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro/java-solutions/expression/*.java prog-intro/java-solutions/expression/parser/*.java
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro/java-solutions/expression/*.java
+javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro/java-solutions/expression/*.java prog-intro/java-solutions/expression/parser/*.java prog-intro/java-solutions/expression/exceptions/*.java prog-intro/java-solutions/expression/generic/**/*.java
 
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/ToMiniString.java
+javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/generic/GenericTester.java
+javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/generic/GenericTest.java
 
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/Expression.java
-
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/ExpressionTest.java
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/ExpressionTester.java
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/TripleExpression.java
-java -ea -cp ./prog-intro/java-solutions:./prog-intro-2024/java expression.TripleExpression hard Triple
-
-
-java -cp ./prog-intro/java-solutions game.Main
-
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/parser/TripleParser.java
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/parser/ParserTest.java
-java -ea -cp ./prog-intro/java-solutions:./prog-intro-2024/java expression.parser.ParserTest easy Base
-
-
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/exceptions/TripleParser.java
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/exceptions/ExceptionsTest.java
-javac -classpath ./prog-intro/java-solutions:./prog-intro-2024/java prog-intro-2024/java/expression/exceptions/ExceptionsTestSet.java
-java -ea -cp ./prog-intro/java-solutions:./prog-intro-2024/java expression.exceptions.ExceptionsTest easy Base
+java -ea -cp ./prog-intro/java-solutions:./prog-intro-2024/java expression.generic.GenericTest easy Base
